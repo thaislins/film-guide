ca696ef553c39255adb45fe19c69dade0ae5109b
Thais Lins <thaislin@gmail.com>
2020-02-09 00:56:36 -0500

    Set click listener for view pager items

--- app/src/main/java/com/thaislins/filmguide/modules/home/view/adapter/ImagePagerAdapter.kt
+++ app/src/main/java/com/thaislins/filmguide/modules/home/view/adapter/ImagePagerAdapter.kt
@@ -1,15 +1,16 @@
 package com.thaislins.filmguide.modules.home.view.adapter
 
 import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.Navigation
 import androidx.viewpager.widget.PagerAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.thaislins.filmguide.BuildConfig
 import com.thaislins.filmguide.R
 import com.thaislins.filmguide.core.BACKDROP_URL
 import com.thaislins.filmguide.modules.home.model.Film
@@ -38,7 +39,23 @@ class ImagePagerAdapter(private val context: Context, var trendingFilms: List<Fi
                 .diskCacheStrategy(DiskCacheStrategy.ALL).into(it)
         }
         container.addView(itemView)
+
+        ivFilm.setOnClickListener {
+            Navigation.findNavController(itemView).navigate(
+                R.id.toDetailsFragment, createBundle(context, position)
+            )
+        }
+
         return itemView
+    }
+
+    private fun createBundle(context: Context, pos: Int): Bundle {
+        val bundle = Bundle()
+        bundle.putParcelable(
+            context.resources.getString(R.string.film_item_key),
+            trendingFilms[pos]
+        )
+        return bundle
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, obj: Any) {
